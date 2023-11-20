8fb48a847d639cc84bc1e2ce934f16c8ca8bd5ab
PandolfiJoaquin <jpandolfi@fi.uba.ar>
2023-11-20 10:54:44 -0300

    terminado input banana

--- ui_src/game_view.cpp
+++ ui_src/game_view.cpp
@@ -379,7 +379,7 @@ void GameView::clickCase(int i, int mouseX, int mouseY) {
 	case BANANA_CODE:
 		this->client.execute(new ThrowGrenade(BANANA,
 			this->currentWormId,
-			dir, angle, 40.0f, 2/3));
+			dir, angle, 40.0f, 3));
 		std::cout << "nanana" << std::endl;
 
 		return;
