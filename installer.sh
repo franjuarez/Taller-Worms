51e646b3b55be824bb2db2f1e238df92b5cf342e
Monke <fjuarez@fi.uba.ar>
2023-12-03 15:42:10 -0300

    agregando instalador de dependencias

---
+++ installer.sh
@@ -0,0 +1,8 @@
+echo "Installing dependencies..."
+sudo apt-get install cmake
+sudo apt-get install libsdl2-dev
+sudo apt-get install libsdl2-image-dev
+sudo apt-get install libsdl2-ttf-dev
+sudo apt-get install libsdl2-mixer-dev
+sudo apt-get install qtbase5-dev
+echo "Done!"
