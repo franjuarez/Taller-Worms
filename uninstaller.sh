3cd3da7f8413e3b376ed343dc591e168a07020ec
Monke <fjuarez@fi.uba.ar>
2023-12-04 10:17:22 -0300

    Mejorando mensajes de instaldor y desinstalador

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,11 +1,13 @@
-echo "uninstalling dependencies..."
+echo "uninstalling..."
 sudo rm -rf /var/TPworms
 sudo rm -rf /etc/TPworms
 sudo rm /usr/bin/wormsServer
 sudo rm /usr/bin/wormsClient
 sudo rm -rf /usr/lib/TPworms
-
 echo "Done!"
 echo "Removing build directory..."
-sudo rm -rf build
-echo "Uninstall complete!"
+if sudo rm -rf build; then
+    echo "Uninstall complete!"
+else    
+    echo "Error: Failed to uninstall. Check the error message above for details."
+fi
