dfb52ca90e14f4d87707f8d34756aa9e39d6f829
Monke <fjuarez@fi.uba.ar>
2023-12-05 10:24:32 -0300

    quitando prints innecesarios de los instaladores

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,7 @@
 echo "Installing Tests..."
 cd build
-cmake -DTESTING=ON .
-cmake --build . -j8
+cmake -DTESTING=ON . > /dev/null 2>&1
+cmake --build . -j8 > /dev/null 2>&1
 echo "Running Tests"
 ctest -rerun-failed --output-on-failure
 echo "All Done!!"
